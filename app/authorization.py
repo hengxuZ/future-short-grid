0eab63b8768b2b90a38a807206a37b974bdf1be3
XingFuCunDeMaNong <m15583730895@163.com>
2021-06-10 11:14:35 +0800

    修改数组越界

--- app/authorization.py
+++ app/authorization.py
@@ -1,7 +1,7 @@
 # Get an Key and Secret
 # https://www.binance.com/restapipub.html
-api_key = 'un1X1hYYeWe8W2qDJQIidHZsA74wS7MLk9uS5qjeXPcFynd29ql5iVuLo3dNjTlS'
-api_secret = 'KTtKQCMuMMozGS3j3iPOYUbg6juWRTTVmSMuYN9qQpQkyOFhs5YYdbaT5yaBqVq3'
+api_key = ''
+api_secret = ''
 recv_window = 5000
 
-dingding_token = "1a3677bdef9d94609d0436a5622a9a4a5420ebd6531ab18f3eef816fbb6714da"
+dingding_token = ""
--- run.py
+++ run.py
@@ -50,10 +50,11 @@ class Run_Main():
 
                 # 平空
                 elif future_sell_price >= cur_market_price and index.calcAngle(coinType, "5m", False,right_size):  # 是否满足卖出价
-                    last_price = runbet.get_record_price(coinType)
-                    sell_amount = runbet.get_future_quantity(coinType,False)
-                    porfit_usdt = (last_price - cur_market_price) / sell_amount # 计算预计盈利多少U
+
                     if future_step > 0:
+                        last_price = runbet.get_record_price(coinType)
+                        sell_amount = runbet.get_future_quantity(coinType, False)
+                        porfit_usdt = (last_price - cur_market_price) / sell_amount  # 计算预计盈利多少U
                         future_res = msg.do_sell_market_msg(coinType, sell_amount, porfit_usdt)  # 期货卖出开多
                         if future_res['orderId']:
                             runbet.set_ratio(coinType)
